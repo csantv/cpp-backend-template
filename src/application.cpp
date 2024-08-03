fce5d81c5c1cba7c040bf743f4a7d6773706bdc3
Carlos Toro <csantve@gmail.com>
2024-08-03 18:53:27 -0500

    add spdlog

--- src/application.cpp
+++ src/application.cpp
@@ -1,13 +1,13 @@
 #include "application.hpp"
 
-#include <iostream>
+#include <spdlog/spdlog.h>
 
 void Application::run() {
     app.get("/*", [] (auto *res, [[maybe_unused]] auto *req) {
         res->end("Hello, World!");
     }).listen(3000, [] (auto *listen_socket) {
         if (listen_socket != nullptr) {
-            std::cout << "listening on port 3000" << std::endl;
+            SPDLOG_INFO("listening on port 3000");
         }
     }).run();
 }
