70db8afeeeb1324e4efb6a16142fcfc1f5fffe76
Carlos Toro <csantve@gmail.com>
2024-08-03 17:41:07 -0500

    fix compilation on win32

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,25 @@
 #include "application.hpp"
-
-#include <csignal>
-#include <iostream>
-
-namespace {
-    void signal_handler(int) {
-        uWS::App app(Application::app.close());
-    }
-}
+//
+// #include <csignal>
+// #include <iostream>
+//
+// namespace {
+//     void signal_handler(int) {
+//         uWS::App app(Application::app.close());
+//     }
+// }
 
 int main()
 {
-    struct sigaction sga {
-    };
-    sga.sa_handler = signal_handler;
-    sigemptyset(&sga.sa_mask);
-    sga.sa_flags = 0;
-    sigaction(SIGINT, &sga, nullptr);
-    sigaction(SIGTERM, &sga, nullptr);
-    sigaction(SIGHUP, nullptr, nullptr);
-    sigaction(SIGCHLD, nullptr, nullptr);
+    // struct sigaction sga {
+    // };
+    // sga.sa_handler = signal_handler;
+    // sigemptyset(&sga.sa_mask);
+    // sga.sa_flags = 0;
+    // sigaction(SIGINT, &sga, nullptr);
+    // sigaction(SIGTERM, &sga, nullptr);
+    // sigaction(SIGHUP, nullptr, nullptr);
+    // sigaction(SIGCHLD, nullptr, nullptr);
 
     Application::run();
     return 0;
